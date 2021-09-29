81062a24daca7bd263fb12a64b189822e15e5668
PHW-H <pancarshwhogendoorn@gmail.com>
2021-09-29 18:01:04 +0100

    added a line

--- Main_R_document.r
+++ Main_R_document.r
@@ -2,3 +2,5 @@ setwd("C:\Users\pancr\OneDrive\Documents\R\Porject_1_Group_44")
 a <- scan("1581-0.txt",what="character",skip=156)
 n <- length(a)
 a <- a[-((n-2909):n)] ## strip license
+
+split_punct
