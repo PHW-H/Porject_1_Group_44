6cf5804bd49dda32baea7bffceacbfbc977b5588
PHW-H <pancarshwhogendoorn@gmail.com>
2021-09-29 17:53:55 +0100

    did some thing

--- Main_R_document.r
+++ Main_R_document.r
@@ -1,1 +1,4 @@
-Main_R_document
+setwd("C:\Users\pancr\OneDrive\Documents\R\Porject_1_Group_44")
+a <- scan("1581-0.txt",what="character",skip=156)
+n <- length(a)
+a <- a[-((n-2909):n)] ## strip license
