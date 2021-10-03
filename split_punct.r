53eb93f97af2d99efe8582d238a3a0c348284f70
PHW-H <pancarshwhogendoorn@gmail.com>
2021-10-03 16:13:00 +0100

    Almost working

--- split_punct.r
+++ split_punct.r
@@ -4,35 +4,37 @@ n <- length(a)
 a <- a[-((n-2909):n)] ## strip license
 
 # find words containting
-punctuation_marks <- c(",",".",";","!",":","?")
+punctuation_marks <- c(",",";","!",":","\\?","\\.")
 
-i=5
+for (i in 1:4) {
+    n <- length(a)
+    c <- grep(punctuation_marks[i],a,fixed = TRUE)
+    lc <- length(c)
 
-c <- grep(punctuation_marks[i],a,fixed = TRUE)
-lc <- length(c)
+    if (lc>0){
+        #create empty string
+        ns <- rep("",2*lc+n)
+        
+        #location of split words
+        rcl <- c+2*(1:lc)
+        ccl <- rcl-1
+        lcl <- rcl-2
+    
+        #split words
+        wlr <- strsplit(a[c], punctuation_marks[i])
+        for (val in 1:lc) {
+            if (length(wlr[[val]])<2) {
+                wlr[[val]] <- c(wlr[[val]],"")
+                }}
+        wrl <- matrix(unlist(wlr), ncol=2, byrow=TRUE)
 
-#create empty string
-ns <- rep("",2*lc+n)
-
-#location of split words
-rcl <- c+2*(1:lc)
-ccl <- rcl-1
-lcl <- rcl-2
-
-#split words
-wlr <- strsplit(a[c], punctuation_marks[i])
-wrl <- ""
-for (val in 1:lc) {wrl <- c(wrl,wlr[[val]])}
-
-#location of unsplit words
-nwl <- c(rcl,ccl,lcl) 
-
-
-ns[ccl] <- punctuation_marks[i]
-ns[lcl] <- wrl[seq(2,lc,2)]
-ns[rcl] <- wrl[seq(3,lc+1,2)]
-ns[-nwl] <- a[-c]
-
-#ns[-nwl] <- an
-rm(wrl)
-print(ns)
+        #location of unsplit words
+        nwl <- c(rcl,ccl,lcl) 
+        ns[ccl] <- punctuation_marks[i]
+        ns[lcl] <- wrl[,1]
+        ns[rcl] <- wrl[,2]
+        ns[-nwl] <- a[-c]
+        rm(wrl)
+        rm(wlr)
+        a <- ns 
+        }}
