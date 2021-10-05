b5d610c37ae94470d14fb42200cd7f069f7cf510
PHW-H <pancarshwhogendoorn@gmail.com>
2021-10-05 16:56:31 +0100

    new improved program

--- split_punct.r
+++ split_punct.r
@@ -51,26 +51,31 @@ a <- a[a != ""]
 
 #part question 6
 
-#remove capital letter
-a_low <- tolower(a)
+a_low <- tolower(a) #lowers capital letters in words
+unique_words <- unique(a_low) #obtains unique words in a
+index_vector <- match(a_low,unique_words) #creates a vector length of a which indicates the word's index in unique_words
+freq <- tabulate(index_vector) #creates a vector length of b where each value corresponds to the frequency of the word with the same index in b
 
-#create list of all different words in a
-un <- unique(a_low)
+th <- 1 #initializing the threshold value to retain 1000 words
+ncw <- length(unique_words) #initializing the number of common words with the given threshold value
+desired_word_count <- 1000 #as requested in the Practical 1 document
 
-#assign numbers to words
-c <- match((a_low),(un))
-#count thow often the words re-occur
-count <- tabulate(c)
+while (ncw >= desired_word_count) {
+  p_th <- th
+  th <- th + 1
+  p_ncw <- ncw
+  ncw <- sum(freq >= th)
+}
 
-#find how often the m most frequent word re-occur (in bbb)
-s_count <- sort(count)
-bbb <- s_count[(length(count)-m+1):length(count)]
+if (desired_word_count - ncw < p_ncw - desired_word_count) {
+  m <- ncw
+  threshold <- th
+} else {
+  m <- p_ncw
+  threshold <- p_th
+}
 
-#find the most m commen used words
-common_words_loc <- count %in% bbb
-common_words <- un[common_words_loc]
-
-b <- common_words
+b <- unique_words[which(freq >= threshold)]
 
 #question 7
 a_index <- match(a_low, b)
