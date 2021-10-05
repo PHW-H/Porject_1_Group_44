d4a8df961467f51a0f6ca4998b70615dba2f79e0
PHW-H <pancarshwhogendoorn@gmail.com>
2021-10-05 16:41:44 +0100

    its wokring

--- split_punct.r
+++ split_punct.r
@@ -51,16 +51,8 @@ a <- a[a != ""]
 
 #part question 6
 
-#eliminate interpunction
-b <- a[a != ":"]
-b <- b[b != "?"]
-b <- b[b != "."]
-b <- b[b != ","]
-b <- b[b != ";"]
-b <- b[b != "!"]
-
 #remove capital letter
-b <- tolower(b)
+b <- tolower(a)
 
 #create list of all different words in a
 un <- unique(b)
@@ -80,3 +72,32 @@ common_words <- un[common_words_loc]
 
 b <- common_words
 
+#question 7
+a_index <- match(a_low, b)
+column_matrix <- cbind(a_index[-length(a_index)],a_index[-1])
+pair_index <- which(!is.na(rowSums(column_matrix)))
+word_pairs <- column_matrix[pair_index,]
+
+m <- length(b)
+
+A <- matrix(0,m,m) # m is the number of common words aka length b
+for (count in 1:nrow(word_pairs)){
+  i <- word_pairs[count,1]
+  j <- word_pairs[count,2]
+  A[i,j] <- A[i,j]+1
+}
+A <- t(apply(A, 1, function(x)(x/sum(x)))) 
+A <- gsub(NaN,1/length(b),A)
+A <- t(matrix(A, ncol=length(b), byrow=TRUE))
+
+#question 8
+prev_word_index <-sample(1:length(b),1)
+end_text_index <- c(prev_word_index)
+
+for (temp in 2:50){
+  chosen_word_index <- sample(1:length(b),1,prob=A[prev_word_index,])
+  end_text_index <- c(end_text_index, chosen_word_index)
+  prev_word_index <- chosen_word_index
+}
+
+print(b[end_text_index])
