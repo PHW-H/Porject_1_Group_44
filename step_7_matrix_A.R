d662c7be9457916d3563a814cb269feeaeef90fb
unknown <cinkilictutku@gmail.com>
2021-10-05 06:24:07 +0100

    covers step 7, returns matrix A

---
+++ step_7_matrix_A.R
@@ -0,0 +1,15 @@
+#assuming a_low is the final a matrix, and b is the array of ~1000 common words
+
+a_index <- match(a_low, b)
+column_matrix <- cbind(a_index[-length(a_index)],a_index[-1])
+na_index <- which(is.na(rowSums(column_matrix)))
+word_pairs <- column_matrix[-na_index,]
+
+A <- matrix(0,m,m) # m is the number of common words aka length b
+for (count in 1:nrow(word_pairs)){
+  i <- word_pairs[count,1]
+  j <- word_pairs[count,2]
+  A[i,j] <- A[i,j]+1
+}
+
+t(apply(A, 1, function(x)(x/sum(x)))) 
